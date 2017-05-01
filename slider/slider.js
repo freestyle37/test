8e992605c1a33ad6fa65239db79f1930eb14ff2a
Your Name <you@example.com>
2017-05-01 14:35:20 +0300

    chto-to nachalo rabotat

--- slider/slider.js
+++ slider/slider.js
@@ -1,11 +1,14 @@
 jQuery(document).ready(function() {
-	var variable;
-	variable=2;		
-	jQuery(".wrapper-for-button button:nth-of-type(1)").on('click',function() {
-		
-		var number;
-		number=jQuery('[value]').val();
+	var variable = 2;		
+	jQuery(".wrapper-for-button button").on('click',function() {
+		console.log(jQuery('[value]'));
+		var number = jQuery(this).val();
 		alert(number);
+		jQuery(".a1").css('z-index', 10);
+		jQuery("body").css({'background':'red', 'border':'1px solid black'});
+
+
+
 
 	});
 
